3d56d67f5f3df1a98a25bfadbfb29f47aad6eb49
Jose Carlos Gomez <josecarlosgomez69gmail.com>
2019-08-07 01:05:05 -0700

    Updates

---
+++ precache-manifest.17919f698153d0e93bfccfa0c50abbf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735ed0ac72da5d47c3bacdcb0f16ee2c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "798021122524f7200d07",
+    "url": "/portfolio/static/css/2.74d8aad0.chunk.css"
+  },
+  {
+    "revision": "ffcb6e17311b06e25ce0",
+    "url": "/portfolio/static/css/main.fc88d2de.chunk.css"
+  },
+  {
+    "revision": "798021122524f7200d07",
+    "url": "/portfolio/static/js/2.a1d41e92.chunk.js"
+  },
+  {
+    "revision": "ffcb6e17311b06e25ce0",
+    "url": "/portfolio/static/js/main.62b4f272.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
